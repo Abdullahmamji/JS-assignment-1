eb6147279a9b462f0f84b0e3517fc31c93aa3e65
Abdullahmamji <117278033+Abdullahmamji@users.noreply.github.com>
2022-11-02 04:53:16 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// alert ('welcome to javascript')
+
+
+// var abc ="welcome to javascript"
+// alert (abc)
+
+var abc ="this is a javascript website"
+alert (abc)
+
+
+// var a = 20;
+// console.log (a);
+
+
+var a = 20;
+var b = 50;
+var c = (a + b) -a;
+console.log (a);
+
+
+var a = 10;
+// a++;
+// a--;\
+// ++a;
+--a; 
+
+console.log (a);
+
+
+var a = 10;
+var b = 20;
